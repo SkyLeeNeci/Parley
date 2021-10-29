3987ace01377b0d4e3efb57065e2994e31739f6b
SkyLeeNeci <92163391+SkyLeeNeci@users.noreply.github.com>
2021-10-29 19:37:04 +0300

    Create setOnClickListener for loginBtn and createAccountBtn

--- app/src/main/java/karpenko/test/parley/LoginActivity.java
+++ app/src/main/java/karpenko/test/parley/LoginActivity.java
@@ -2,13 +2,26 @@ package karpenko.test.parley;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText emailBox, passwordBox;
+    Button loginBtn, createAccountBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailBox = findViewById(R.id.emailBox);
+        passwordBox = findViewById(R.id.passwordBox);
+        loginBtn = findViewById(R.id.loginbtn);
+        createAccountBtn = findViewById(R.id.createAccoutbtn);
+
+        createAccountBtn.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, SignUpActivity.class)));
+
     }
 }
--- app/src/main/java/karpenko/test/parley/SignUpActivity.java
+++ app/src/main/java/karpenko/test/parley/SignUpActivity.java
@@ -2,13 +2,28 @@ package karpenko.test.parley;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText userName,userEmail, userPassword;
+    Button createAccountBtn, logInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        userName = findViewById(R.id.userNameBox);
+        userEmail = findViewById(R.id.emailBox);
+        userPassword = findViewById(R.id.passwordBox);
+        createAccountBtn = findViewById(R.id.createAccoutbtn);
+        logInBtn = findViewById(R.id.loginbtn);
+
+        logInBtn.setOnClickListener(v -> startActivity( new Intent(SignUpActivity.this,LoginActivity.class)));
+
     }
 }
