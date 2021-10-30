fd4958df62e00f744c89afc4775f176555e898f6
SkyLeeNeci <92163391+SkyLeeNeci@users.noreply.github.com>
2021-10-30 16:57:39 +0300

    Create activity_dashboard.xml and connect for it.

--- app/src/main/java/karpenko/test/parley/LoginActivity.java
+++ app/src/main/java/karpenko/test/parley/LoginActivity.java
@@ -1,16 +1,25 @@
 package karpenko.test.parley;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText emailBox, passwordBox;
     Button loginBtn, createAccountBtn;
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,8 +29,28 @@ public class LoginActivity extends AppCompatActivity {
         passwordBox = findViewById(R.id.passwordBox);
         loginBtn = findViewById(R.id.loginbtn);
         createAccountBtn = findViewById(R.id.createAccoutbtn);
+        auth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(v -> {
+
+            String email, password;
+            email = emailBox.getText().toString();
+            password = passwordBox.getText().toString();
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(LoginActivity.this,DashboardActivity.class));
+                    Toast.makeText(LoginActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        });
 
         createAccountBtn.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, SignUpActivity.class)));
+
+
 
     }
 }
--- app/src/main/java/karpenko/test/parley/SignUpActivity.java
+++ app/src/main/java/karpenko/test/parley/SignUpActivity.java
@@ -1,13 +1,20 @@
 package karpenko.test.parley;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -15,6 +22,7 @@ public class SignUpActivity extends AppCompatActivity {
     Button createAccountBtn, logInBtn;
 
     FirebaseAuth auth;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +34,37 @@ public class SignUpActivity extends AppCompatActivity {
         userPassword = findViewById(R.id.passwordBox);
         createAccountBtn = findViewById(R.id.createAccoutbtn);
         logInBtn = findViewById(R.id.loginbtn);
+        auth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         logInBtn.setOnClickListener(v -> startActivity( new Intent(SignUpActivity.this,LoginActivity.class)));
+
+
+
+        createAccountBtn.setOnClickListener(v -> {
+            String email, name , password;
+            name = userName.getText().toString();
+            email = userEmail.getText().toString();
+            password = userPassword.getText().toString();
+
+            User user = new User();
+
+            user.setName(name);
+            user.setEmail(email);
+            user.setPassword(password);
+
+            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    firebaseFirestore.collection("users")
+                            .document()
+                            .set(user).addOnSuccessListener(unused ->
+                            startActivity(new Intent(SignUpActivity.this,LoginActivity.class)));
+                    Toast.makeText(SignUpActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(SignUpActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
 
     }
 }
