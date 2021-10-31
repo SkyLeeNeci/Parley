d95e105733c1d1e57ee24418dd345fb1d39bc147
SkyLeeNeci <92163391+SkyLeeNeci@users.noreply.github.com>
2021-10-31 17:22:14 +0200

    add jitsi dependency

--- app/src/main/java/karpenko/test/parley/DashboardActivity.java
+++ app/src/main/java/karpenko/test/parley/DashboardActivity.java
@@ -3,12 +3,42 @@ package karpenko.test.parley;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    EditText secretCode;
+    Button connectBtn, shareBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        secretCode = findViewById(R.id.secreatCodeBox);
+        connectBtn = findViewById(R.id.connectBtn);
+        shareBtn = findViewById(R.id.shareLinkBtn);
+
+        URL url;
+        try {
+            url = new URL("https://meet.jit.si");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+        JitsiMeetConferenceOptions jitsiMeetConferenceOptions;
+
+        connectBtn.setOnClickListener(v ->{
+
+        });
     }
 }
