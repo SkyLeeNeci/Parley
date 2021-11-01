24788083d2ec59b16e1c1822f6dc1b5bb81b1ff4
SkyLeeNeci <92163391+SkyLeeNeci@users.noreply.github.com>
2021-11-01 21:07:28 +0200

    menu buttons setOnClickListener

--- app/src/main/java/karpenko/test/parley/DashboardActivity.java
+++ app/src/main/java/karpenko/test/parley/DashboardActivity.java
@@ -1,10 +1,17 @@
 package karpenko.test.parley;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.jitsi.meet.sdk.JitsiMeet;
 import org.jitsi.meet.sdk.JitsiMeetActivity;
@@ -17,6 +24,8 @@ public class DashboardActivity extends AppCompatActivity {
 
     EditText secretCode;
     Button connectBtn, shareBtn;
+    BottomNavigationItemView home,settings,history,logOut;
+    CardView cardViewHome, cardViewSettings, cardViewHistory;
 
 
 
@@ -28,6 +37,13 @@ public class DashboardActivity extends AppCompatActivity {
         secretCode = findViewById(R.id.secreatCodeBox);
         connectBtn = findViewById(R.id.connectBtn);
         shareBtn = findViewById(R.id.shareLinkBtn);
+        home = findViewById(R.id.homeBtn);
+        settings = findViewById(R.id.settingsBtn);
+        history = findViewById(R.id.historyBtn);
+        logOut = findViewById(R.id.logOutBtn);
+        cardViewHistory = findViewById(R.id.cardViewHistory);
+        cardViewHome = findViewById(R.id.cardViewHome);
+        cardViewSettings = findViewById(R.id.cardViewSettings);
 
         URL url;
         try {
@@ -45,6 +61,33 @@ public class DashboardActivity extends AppCompatActivity {
                     .setWelcomePageEnabled(false).build();
 
             JitsiMeetActivity.launch(DashboardActivity.this,options);
+        });
+
+        settings.setOnClickListener(v -> {
+
+            cardViewHome.setVisibility(View.INVISIBLE);
+            cardViewHistory.setVisibility(View.INVISIBLE);
+            cardViewSettings.setVisibility(View.VISIBLE);
+        });
+
+        home.setOnClickListener(v -> {
+
+            cardViewHome.setVisibility(View.VISIBLE);
+            cardViewHistory.setVisibility(View.INVISIBLE);
+            cardViewSettings.setVisibility(View.INVISIBLE);
+        });
+
+        history.setOnClickListener(v -> {
+
+            cardViewHome.setVisibility(View.INVISIBLE);
+            cardViewHistory.setVisibility(View.VISIBLE);
+            cardViewSettings.setVisibility(View.INVISIBLE);
+        });
+
+        logOut.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(DashboardActivity.this,LoginActivity.class));
+            finish();
         });
     }
 }
