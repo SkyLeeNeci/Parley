f7747afc40099fed34d9981e4eda3e3be8c32f1a
SkyLeeNeci <92163391+SkyLeeNeci@users.noreply.github.com>
2021-11-05 18:14:47 +0200

    create delete account btn

--- app/src/main/java/karpenko/test/parley/SettingsActivity.java
+++ app/src/main/java/karpenko/test/parley/SettingsActivity.java
@@ -1,18 +1,26 @@
 package karpenko.test.parley;
 
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingsActivity extends AppCompatActivity {
 
 
     TextView passChange, deleteAcc;
     BottomNavigationItemView home,settings,history,logOut;
+    FirebaseAuth auth;
+    FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,8 @@ public class SettingsActivity extends AppCompatActivity {
         settings = findViewById(R.id.settingsBtn);
         history = findViewById(R.id.historyBtn);
         logOut = findViewById(R.id.logOutBtn);
-
+        auth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
 
         home.setOnClickListener(v -> startActivity(new Intent(SettingsActivity.this,DashboardActivity.class)));
 
@@ -38,6 +47,28 @@ public class SettingsActivity extends AppCompatActivity {
             startActivity(new Intent(SettingsActivity.this,LoginActivity.class));
             FirebaseAuth.getInstance().signOut();
             finishAffinity();
+        });
+
+        history.setOnClickListener(v -> Toast.makeText(SettingsActivity.this, "The history of the rooms is not yet available.", Toast.LENGTH_SHORT).show());
+
+        passChange.setOnClickListener(v -> startActivity(new Intent(SettingsActivity.this,ChangePassActivity.class)));
+
+        deleteAcc.setOnClickListener(v ->{
+            AlertDialog.Builder dialog = new AlertDialog.Builder(SettingsActivity.this);
+            dialog.setTitle("Are you sure?");
+            dialog.setMessage("Remove your account from the system?");
+            dialog.setPositiveButton("Yes", (dialog1, which) -> user.delete().addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+                    Toast.makeText(SettingsActivity.this, "Deleted", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(SettingsActivity.this,LoginActivity.class));
+                    finishAffinity();
+                }else {
+                    Toast.makeText(SettingsActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }));
+            dialog.setNegativeButton("No", (dialog12, which) -> dialog12.dismiss());
+            AlertDialog alertDialog = dialog.create();
+            alertDialog.show();
         });
 
     }
