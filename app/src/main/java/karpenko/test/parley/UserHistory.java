fe42f2b81f7893c8bef843e2689d673df506c29d
SkyLeeNeci <92163391+SkyLeeNeci@users.noreply.github.com>
2021-11-12 19:14:59 +0200

    try to get info from firebase

--- app/src/main/java/karpenko/test/parley/UserHistory.java
+++ app/src/main/java/karpenko/test/parley/UserHistory.java
@@ -1,14 +1,54 @@
 package karpenko.test.parley;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class UserHistory extends AppCompatActivity {
+
+    private static final String TAG = "Read from db";
+    TextView history;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_history);
+
+        history = findViewById(R.id.userHistoryInfo);
+        auth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        db.collection("userVisitedRoom").whereEqualTo("userID", user.getUid())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String date = document.get("date", String.class);
+                                String name = document.get("roomCode", String.class);
+                                history.setText(name + " " + date);
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
     }
 }
